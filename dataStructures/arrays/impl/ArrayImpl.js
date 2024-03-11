6d36f2df772f34fcad8de591d5178f5979493bc4
raiyanmook27 <raiyan.mook27@gmail.com>
2024-03-11 06:32:04 +0000

    QA

--- dataStructures/arrays/impl/ArrayImpl.js
+++ dataStructures/arrays/impl/ArrayImpl.js
@@ -35,6 +35,9 @@ class ArrayImpl {
     for (let i = index; i <= this.length - 1; i++) {
       this.data[i] = this.data[i + 1];
     }
+
+    delete this.data[this.length - 1];
+
     this.length--;
 
     return this;
